877e05280d0cafd3d818c0c7ef61f0323ae6960b
Елизавета Кулешова <elizabeth.kuleshova@gmail.com>
2022-02-27 17:57:48 +0700

    Добавлен openCell и setFlag

--- src/GameMap.ts
+++ src/GameMap.ts
@@ -1,8 +1,10 @@
 import { BombField } from "./BombField";
 
 export enum GameMapCell {
-    Closed=-1, Flagged, Opened0, Opened1, Opened2, Opened3, Opened4, Opened5, Opened6, Opened7, Opened8, Boom
+   Closed = -1, Flagged, Opened0, Opened1, Opened2, Opened3, Opened4, Opened5, Opened6, Opened7, Opened8, Boom
 }
+
+export enum FlagResult { FlagAdded = -1, NoChanges = 0, FlagRemoved = 1 }
 
 export class GameMap {
     private bombField: BombField;
@@ -13,9 +15,84 @@ export class GameMap {
 
         const row = new Array(width);
         row.fill(GameMapCell.Closed);
-        
+
         this.cells = new Array(height);
         this.cells.fill(row);
+    }
+
+    setFlag(x: number, y: number): FlagResult {
+        if (this.cells[y][x] == GameMapCell.Closed) {
+            this.cells[y][x] = GameMapCell.Flagged;
+            return FlagResult.FlagAdded;
+        } else { 
+            return FlagResult.NoChanges;
+        }
+
+    }
+
+    removeFlag(x: number, y: number): FlagResult {
+        this.cells[y][x] = GameMapCell.Closed;
+        return FlagResult.FlagRemoved;
+    }
+
+    //removeFlag() { throw new Error("Not implemented"); }
+
+    getCell(x: number, y: number): GameMapCell {
+        if (y < 0 || y >= this.cells.length) {
+            throw new Error(`Cell coordinates (${x}, ${y}) is out of game map boundaries.`);
+        }
+
+        if (x < 0 || this.cells.length == 0 || x >= this.cells[0].length) {
+            throw new Error(`Cell coordinates (${x}, ${y}) is out of game map boundaries.`);
+        }
+
+        return this.cells[y][x];
+    }
+
+    openCell(x: number, y: number): GameMapCell {
+        if (this.cells[y][x] != GameMapCell.Closed) {
+            return this.cells[y][x];
+        }
+        if (this.bombField.exists([x,y])) {
+            this.cells[y][x] = GameMapCell.Boom;
+    
+        } else {
+            const neighboursCount = this.bombField.calculateNeighbours([x,y]);
+            switch(neighboursCount) {
+                case 0: 
+                    this.cells[y][x] = GameMapCell.Opened0; 
+                    break;
+                case 1:
+                    this.cells[y][x] = GameMapCell.Opened1; 
+                    break;
+                case 2:
+                    this.cells[y][x] = GameMapCell.Opened2; 
+                    break;
+                case 3:
+                    this.cells[y][x] = GameMapCell.Opened3; 
+                    break;                       
+                case 4:
+                    this.cells[y][x] = GameMapCell.Opened4; 
+                    break;                       
+                case 5:
+                    this.cells[y][x] = GameMapCell.Opened5; 
+                    break;
+                case 6:
+                    this.cells[y][x] = GameMapCell.Opened6; 
+                    break;                       
+                case 7:
+                    this.cells[y][x] = GameMapCell.Opened7; 
+                    break;
+                case 8:
+                    this.cells[y][x] = GameMapCell.Opened8; 
+                    break; 
+                default:   
+                    throw new Error("Неправильное количество соседей");
+            }
+
+        }
+
+        return this.cells[y][x];
     }
 
     getCells = () => this.cells;
--- tests/GameMap.test.ts
+++ tests/GameMap.test.ts
@@ -1,7 +1,7 @@
 import { BombField } from "../src/BombField"
-import { GameMap, GameMapCell } from "../src/GameMap";
+import { FlagResult, GameMap, GameMapCell } from "../src/GameMap";
 
-describe("Игровое поле", () => {
+describe("Создание игрового поля", () => {
 
     it("Количество ячеек совпадет с заказанным", () => {
         const width = 5, height = 8;
@@ -20,13 +20,101 @@ describe("Игровое поле", () => {
 
     })
 
-    it("Новое игровое поле должно создаваться с закрытыми клетками", () => {
+    it("Новое игровое поле должно создаваться с закрытыми ячейками", () => {
         const bombField = new BombField();
         const gameMap = new GameMap(bombField, 10, 13);
         const result = gameMap.getCells().flat().every(x => x == GameMapCell.Closed);
         expect(result).toBeTruthy();
 
     })
+})
 
+describe("Открытие ячейки игрового поля", () => {
+    it("Если попытаться открыть уже открытую ячейку, ничего не происходит", () => {
+        const bombField = new BombField();
+        const gameMap = new GameMap(bombField, 5, 6);
+
+        const noChangesCellsValues: GameMapCell[] = [GameMapCell.Flagged, 
+                                                    GameMapCell.Opened0, 
+                                                    GameMapCell.Opened1, 
+                                                    GameMapCell.Opened2,
+                                                    GameMapCell.Opened3, 
+                                                    GameMapCell.Opened4, 
+                                                    GameMapCell.Opened5, 
+                                                    GameMapCell.Opened6, 
+                                                    GameMapCell.Opened7, 
+                                                    GameMapCell.Opened8, 
+                                                    GameMapCell.Boom];
+        
+        const x = 0, y = 0;
+        noChangesCellsValues.forEach(cellValue => {
+            gameMap.getCells()[y][x] = cellValue;
+
+            const result = gameMap.openCell(x, y);
+
+            expect(result).toBe(cellValue);
+            expect(gameMap.getCell(x, y)).toBe(cellValue);
+        });
+    });
+    
+    it.todo("Если открыть ячейку, в котором нет мин и есть хоть одна мина по соседству, записать в ячейку кол-во соседей"); 
+    it.todo("Если открыть ячейку, в котором нет мин и нет мин по соседству, открыть все такие ячейки вокруг"); 
+    it.todo("Если открыть ячейку с миной, пометить её взрывом"); 
+    it.todo("Если открыть ячейку с миной, метод возвращает соответствующее значение");     
+})
+
+describe("Работа с флагами", () => {
+    it("Мы успешно ставим флаг на закрытую ячейку", () => {
+        const bombField = new BombField();
+        const gameMap = new GameMap(bombField, 10, 13);
+        
+        const x = 0, y = 0;
+        const result = gameMap.setFlag(x, y);
+
+        expect(result).toBe(FlagResult.FlagAdded);
+        expect(gameMap.getCell(x, y)).toBe(GameMapCell.Flagged);
+    });
+    
+    it("Мы успешно можем снять флаг с ячейки с флагом", () => {
+        const bombField = new BombField();
+        const gameMap = new GameMap(bombField, 5, 6);
+        
+        const x = 0, y = 0;
+        gameMap.setFlag(x, y);
+
+        const result = gameMap.removeFlag(x, y);
+
+        expect(result).toBe(FlagResult.FlagRemoved);
+        expect(gameMap.getCell(x, y)).toBe(GameMapCell.Closed);
+    });
+
+    it("Если попыться поставить флаг на незакрытую ячейку, ничего не происходит", () => {
+
+        const bombField = new BombField();
+        const gameMap = new GameMap(bombField, 5, 6);
+
+        const noChangesCellsValues: GameMapCell[] = [GameMapCell.Flagged, 
+                                                    GameMapCell.Opened0, 
+                                                    GameMapCell.Opened1, 
+                                                    GameMapCell.Opened2,
+                                                    GameMapCell.Opened3, 
+                                                    GameMapCell.Opened4, 
+                                                    GameMapCell.Opened5, 
+                                                    GameMapCell.Opened6, 
+                                                    GameMapCell.Opened7, 
+                                                    GameMapCell.Opened8, 
+                                                    GameMapCell.Boom];
+        
+        const x = 0, y = 0;
+        noChangesCellsValues.forEach(cellValue => {
+            gameMap.getCells()[y][x] = cellValue;
+
+            const result = gameMap.setFlag(x, y);
+
+            expect(result).toBe(FlagResult.NoChanges);
+            expect(gameMap.getCell(x, y)).toBe(cellValue);
+        })
+
+    }); 
 
 })
