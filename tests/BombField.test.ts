24caab7e440143095c77d5b362b1a70298c1eaed
Елизавета Кулешова <elizabeth.kuleshova@gmail.com>
2022-06-13 13:25:14 +0700

    Изменены правила

--- tests/BombField.test.ts
+++ tests/BombField.test.ts
@@ -1,45 +1,51 @@
 import { BombField } from "../src/BombField";
 
-describe("Генератор минного поля", ()=>{
+describe("Минное поле", ()=>{
 
-    it("должен требовать количество мин не менее 1", () => {
-        
+    describe("Создание минного поля", () => {
+
         const width = 100, height = 150;
 
-        expect(()=>{
-            const field = BombField.generate(1, width, height); 
-        }).not.toThrow();
+        describe("Когда мы указываем разное количество мин при генерации", () => {
+            it("должно успешно создаться при запросе поля на количество мин меньшего, чем количество клеток", () => {
+                expect(()=>{
+                    BombField.generate(10, width, height); 
+                }).not.toThrow();
+            })
+    
+            it("должно успешно создаться при запросе поля на количество мин равное количеству ячеек", () => {
+                expect(()=>{
+                    BombField.generate(width*height, width, height); 
+                }).not.toThrow();
+            })
+     
+            it("должно успешно создаться при запросе поля на 1 мину", () => {
+                expect(()=>{
+                    BombField.generate(1, width, height); 
+                }).not.toThrow();    
+            })
+    
+            it("должно бросить ошибку при запросе поля на 0 мин", () => {
+                expect(()=>{
+                    BombField.generate(0, width, height); 
+                }).toThrow();
+            })
+    
+            it("должно бросить ошибку при запросе поля на отрицательное количество мин", () => {
+                expect(()=>{
+                    BombField.generate(-10, width, height); 
+                }).toThrow();
+            })
+    
+            it("должно бросить ошибку при количестве мин, превышающем кол-во ячеек", () => {
+                expect(()=>{
+                    BombField.generate(width*height + 1, width, height); 
+                }).toThrow();
+            })    
+        })
 
-        expect(()=>{
-            const field = BombField.generate(10, width, height); 
-        }).not.toThrow();
-
-        expect(()=>{
-            const field = BombField.generate(0, width, height); 
-        }).toThrow();
-
-        expect(()=>{
-            const field = BombField.generate(-10, width, height); 
-        }).toThrow();        
     })
 
-    it("должен требовать количество мин не больше ширины*высоту", () => {
-        
-        const width = 10, height = 15;
-        const maxPossibleAmount = width*height;
-
-        expect(()=>{
-            const field = BombField.generate(30, width, height);
-        }).not.toThrow();
-
-        expect(()=>{
-            const field = BombField.generate(maxPossibleAmount, width, height);
-        }).not.toThrow();
-
-        expect(()=>{
-            const field = BombField.generate(maxPossibleAmount + 1, width, height);
-        }).toThrow();
-    })    
 
     it("должен сгенерировать количество мин столько, сколько мы заказали", ()=>{
         const amount = 5;
@@ -67,184 +73,188 @@ describe("Генератор минного поля", ()=>{
 
 describe("Методы минного поля", ()=>{
 
-    it("метод проверки возвращает true если передать координаты существующей бомбы" , ()=>{
-        const field = new BombField();
+    describe("Методы проверки", () => {
+        it("возвращает true если передать координаты существующей бомбы" , ()=>{
+            const field = new BombField();
+    
+            const bomb1: [number, number] = [50, 30];
+            const bomb2: [number, number] = [40, 70];
+    
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+    
+            expect(field.exists(bomb1)).toBeTruthy();
+            expect(field.exists(bomb2)).toBeTruthy();
+        })
 
-        const bomb1: [number, number] = [50, 30];
-        const bomb2: [number, number] = [40, 70];
-
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
-
-        expect(field.exists(bomb1)).toBeTruthy();
-        expect(field.exists(bomb2)).toBeTruthy();
+        it("возвращает false если передать координаты отсутствующей бомбы" , () => {
+            const field = new BombField();
+    
+            const bomb1: [number, number] = [50, 30];
+            const bomb2: [number, number] = [40, 70];
+            const nonExistingBomb: [number, number] = [42, 42];
+    
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+    
+            expect(field.exists(nonExistingBomb)).toBeFalsy();
+        })
     })
 
-    it("метод проверки возвращает false если передать координаты отсутствующей бомбы" , () => {
-        const field = new BombField();
+    describe("Методы вычисления", () => {
 
-        const bomb1: [number, number] = [50, 30];
-        const bomb2: [number, number] = [40, 70];
-        const nonExistingBomb: [number, number] = [42, 42];
+        it("возвращает 0 если рядом нет ни одной мины", () => {
+            
+            const field = new BombField();
 
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
+            const bomb1: [number, number] = [10, 10];
+            const bomb2: [number, number] = [10, 9];
 
-        expect(field.exists(nonExistingBomb)).toBeFalsy();
-    })
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
 
-    it("метод вычисления соседей возвращает 0 если рядом нет ни одной мины", () => {
-        
-        const field = new BombField();
+            expect(field.calculateNeighbours([1,1])).toBe(0);        
+        })
 
-        const bomb1: [number, number] = [10, 10];
-        const bomb2: [number, number] = [10, 9];
+        it("возвращает 8 если клетка окружена минами", () => {
+            
+            const field = new BombField();
 
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
+            const bomb1: [number, number] = [0, 0];
+            const bomb2: [number, number] = [0, 1];
+            const bomb3: [number, number] = [0, 2];
+            const bomb4: [number, number] = [1, 0];
+            const bomb5: [number, number] = [1, 2];
+            const bomb6: [number, number] = [2, 0];
+            const bomb7: [number, number] = [2, 1];
+            const bomb8: [number, number] = [2, 2];
 
-        expect(field.calculateNeighbours([1,1])).toBe(0);        
-    })
+            /* 
+            * * *
+            * ? *   -> 8
+            * * *
+            */
 
-    it("метод вычисления соседей возвращает 8 если клетка окружена минами", () => {
-        
-        const field = new BombField();
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+            field.getBombs().push(bomb3);
+            field.getBombs().push(bomb4);
+            field.getBombs().push(bomb5);
+            field.getBombs().push(bomb6);
+            field.getBombs().push(bomb7);
+            field.getBombs().push(bomb8);
 
-        const bomb1: [number, number] = [0, 0];
-        const bomb2: [number, number] = [0, 1];
-        const bomb3: [number, number] = [0, 2];
-        const bomb4: [number, number] = [1, 0];
-        const bomb5: [number, number] = [1, 2];
-        const bomb6: [number, number] = [2, 0];
-        const bomb7: [number, number] = [2, 1];
-        const bomb8: [number, number] = [2, 2];
+            expect(field.calculateNeighbours([1,1])).toBe(8);        
+        })
 
-        /* 
-           * * *
-           * ? *   -> 8
-           * * *
-        */
+        // Ещё пара случаев (соседи по диагонали, соседи слева/справа/сверху/снизу)
 
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
-        field.getBombs().push(bomb3);
-        field.getBombs().push(bomb4);
-        field.getBombs().push(bomb5);
-        field.getBombs().push(bomb6);
-        field.getBombs().push(bomb7);
-        field.getBombs().push(bomb8);
+        it("возвращает 4 если во всех диагональных направлениях есть мины", () => {
+            
+            const field = new BombField();
 
-        expect(field.calculateNeighbours([1,1])).toBe(8);        
-    })
+            const bomb1: [number, number] = [0, 0];
+            const bomb2: [number, number] = [0, 2];
+            const bomb3: [number, number] = [2, 0];
+            const bomb4: [number, number] = [2, 2];
 
-    // Ещё пара случаев (соседи по диагонали, соседи слева/справа/сверху/снизу)
+            /* 
+            * - *
+            - ? -   -> 4
+            * - *
+            */
 
-    it("метод вычисления соседей возвращает 4 если во всех диагональных направлениях есть мины", () => {
-        
-        const field = new BombField();
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+            field.getBombs().push(bomb3);
+            field.getBombs().push(bomb4);
+            
+            expect(field.calculateNeighbours([1,1])).toBe(4);        
+        })
 
-        const bomb1: [number, number] = [0, 0];
-        const bomb2: [number, number] = [0, 2];
-        const bomb3: [number, number] = [2, 0];
-        const bomb4: [number, number] = [2, 2];
+        it("возвращает 3 если справа во всех клетках бомбы", () => {
+            
+            const field = new BombField();
 
-        /* 
-           * - *
-           - ? -   -> 4
-           * - *
-        */
+            const bomb1: [number, number] = [2, 0];
+            const bomb2: [number, number] = [2, 1];
+            const bomb3: [number, number] = [2, 2];
 
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
-        field.getBombs().push(bomb3);
-        field.getBombs().push(bomb4);
-        
-        expect(field.calculateNeighbours([1,1])).toBe(4);        
-    })
+            /* 
+            - - *
+            - ? *   -> 3
+            - - *
+            */
 
-    it("метод вычисления соседей возвращает 3 если справа во всех клетках бомбы", () => {
-        
-        const field = new BombField();
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+            field.getBombs().push(bomb3);
+            
+            expect(field.calculateNeighbours([1,1])).toBe(3);        
+        })
 
-        const bomb1: [number, number] = [2, 0];
-        const bomb2: [number, number] = [2, 1];
-        const bomb3: [number, number] = [2, 2];
+        it("возвращает 3 если снизу во всех клетках бомбы", () => {
+            
+            const field = new BombField();
 
-        /* 
-           - - *
-           - ? *   -> 3
-           - - *
-        */
+            const bomb1: [number, number] = [0, 0];
+            const bomb2: [number, number] = [1, 0];
+            const bomb3: [number, number] = [2, 0];
 
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
-        field.getBombs().push(bomb3);
-        
-        expect(field.calculateNeighbours([1,1])).toBe(3);        
-    })
+            /* 
+            - - -
+            - ? -   -> 3
+            * * *
+            */
 
-    it("метод вычисления соседей возвращает 3 если снизу во всех клетках бомбы", () => {
-        
-        const field = new BombField();
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+            field.getBombs().push(bomb3);
+            
+            expect(field.calculateNeighbours([1,1])).toBe(3);        
+        })
 
-        const bomb1: [number, number] = [0, 0];
-        const bomb2: [number, number] = [1, 0];
-        const bomb3: [number, number] = [2, 0];
+        // Тесты на сценарии, когда проверяем у края (как минимум у левого и верхнего, можно и право/низ)
+            
 
-        /* 
-           - - -
-           - ? -   -> 3
-           * * *
-        */
+        it("возвращает 3 если вокруг клетки 3 мины (левый край и еще одна)", () => {
+            
+            const field = new BombField();
 
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
-        field.getBombs().push(bomb3);
-        
-        expect(field.calculateNeighbours([1,1])).toBe(3);        
-    })
+            const bomb1: [number, number] = [1, 3];
+            const bomb2: [number, number] = [1, 1];
+            const bomb3: [number, number] = [2, 1];
+            
+            /* 
+            * - -
+            ? - -   -> 3
+            * * -
+            */ 
 
-    // Тесты на сценарии, когда проверяем у края (как минимум у левого и верхнего, можно и право/низ)
-        
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+            field.getBombs().push(bomb3);
+            
+            expect(field.calculateNeighbours([1,2])).toBe(3);        
+        })
 
-    it("метод вычисления соседей возвращает 3 если вокруг клетки 3 мины (левый край и еще одна)", () => {
-        
-        const field = new BombField();
+        it("возвращает 2 если вокруг клетки 2 мины (нижний край)" , () => {
+            
+            const field = new BombField();
 
-        const bomb1: [number, number] = [1, 3];
-        const bomb2: [number, number] = [1, 1];
-        const bomb3: [number, number] = [2, 1];
-        
-        /* 
-           * - -
-           ? - -   -> 3
-           * * -
-        */ 
+            const bomb1: [number, number] = [0, 0];
+            const bomb2: [number, number] = [2, 0];
+            
+            /* 
+            - - -
+            - - -   -> 2
+            * ? *
+            */ 
 
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
-        field.getBombs().push(bomb3);
-        
-        expect(field.calculateNeighbours([1,2])).toBe(3);        
-    })
-
-    it("метод вычисления соседей возвращает 2 если вокруг клетки 2 мины (нижний край)" , () => {
-        
-        const field = new BombField();
-
-        const bomb1: [number, number] = [0, 0];
-        const bomb2: [number, number] = [2, 0];
-        
-        /* 
-           - - -
-           - - -   -> 2
-           * ? *
-        */ 
-
-        field.getBombs().push(bomb1);
-        field.getBombs().push(bomb2);
-        
-        expect(field.calculateNeighbours([1,0])).toBe(2);        
-    })
-
+            field.getBombs().push(bomb1);
+            field.getBombs().push(bomb2);
+            
+            expect(field.calculateNeighbours([1,0])).toBe(2);        
+        })
+    })   
 })
--- tests/GameMap.test.ts
+++ tests/GameMap.test.ts
@@ -30,51 +30,201 @@ describe("Создание игрового поля", () => {
 })
 
 describe("Открытие ячейки игрового поля", () => {
-    it("Если попытаться открыть уже открытую ячейку, ничего не происходит", () => {
+
+    describe("Когда открываем незакрытую ячейку", () => {
         const bombField = new BombField();
         const gameMap = new GameMap(bombField, 5, 6);
-
-        const noChangesCellsValues: GameMapCell[] = [GameMapCell.Flagged, 
-                                                    GameMapCell.Opened0, 
-                                                    GameMapCell.Opened1, 
-                                                    GameMapCell.Opened2,
-                                                    GameMapCell.Opened3, 
-                                                    GameMapCell.Opened4, 
-                                                    GameMapCell.Opened5, 
-                                                    GameMapCell.Opened6, 
-                                                    GameMapCell.Opened7, 
-                                                    GameMapCell.Opened8, 
-                                                    GameMapCell.Boom];
-        
         const x = 0, y = 0;
-        noChangesCellsValues.forEach(cellValue => {
+    
+        it("ячейка c флагом остаётся флагом", () => {
+            const cellValue = GameMapCell.Flagged;             
             gameMap.getCells()[y][x] = cellValue;
-
+    
             const result = gameMap.openCell(x, y);
 
             expect(result).toBe(cellValue);
-            expect(gameMap.getCell(x, y)).toBe(cellValue);
-        });
-    });
+            expect(gameMap.getCell(x, y)).toBe(cellValue);        
+        })
+
+        it("взорванная ячейка должна остаться взорванной", () => {
+            const cellValue = GameMapCell.Boom;             
+            gameMap.getCells()[y][x] = cellValue;
     
-    it.todo("Если открыть ячейку, в котором нет мин и есть хоть одна мина по соседству, записать в ячейку кол-во соседей"); 
-    it.todo("Если открыть ячейку, в котором нет мин и нет мин по соседству, открыть все такие ячейки вокруг"); 
-    it.todo("Если открыть ячейку с миной, пометить её взрывом"); 
-    it.todo("Если открыть ячейку с миной, метод возвращает соответствующее значение");     
+            const result = gameMap.openCell(x, y);
+
+            expect(result).toBe(cellValue);
+            expect(gameMap.getCell(x, y)).toBe(cellValue);        
+        })
+
+        it("открытая ячейка остаётся открытой и количество соседей не меняется", () => {
+            const noChangesCellsValues: GameMapCell[] = [ 
+                GameMapCell.Opened0, 
+                GameMapCell.Opened1, 
+                GameMapCell.Opened2,
+                GameMapCell.Opened3, 
+                GameMapCell.Opened4, 
+                GameMapCell.Opened5, 
+                GameMapCell.Opened6, 
+                GameMapCell.Opened7, 
+                GameMapCell.Opened8];
+
+            noChangesCellsValues.forEach(cellValue => {
+                gameMap.getCells()[y][x] = cellValue;
+    
+                const result = gameMap.openCell(x, y);
+    
+                expect(result).toBe(cellValue);
+                expect(gameMap.getCell(x, y)).toBe(cellValue);            
+            });
+        })
+    })
+
+    describe("Когда открываем закрытую ячейку", () => {
+
+        const bombField = new BombField();
+        const gameMap = new GameMap(bombField, 5, 6);
+        const x = 0, y = 0;
+
+        let spy: jest.SpyInstance<any, any> | null = null;
+
+        describe("Без мины и рядом одна мина", () => {
+            gameMap.getCells()[y][x] = GameMapCell.Closed;    
+            spy = jest.spyOn(bombField, 'calculateNeighbours').mockImplementation(_ => 1);
+
+            const result = gameMap.openCell(x, y);
+
+            it("в ячейку должно записаться 1", () =>{
+                expect(result).toBe(GameMapCell.Opened1)
+            })
+        })
+
+        describe("Без мины и рядом нет мин", () => {
+            gameMap.getCells()[y][x] = GameMapCell.Closed;    
+            spy = jest.spyOn(bombField, 'calculateNeighbours').mockImplementation(_ => 0);
+
+            const result = gameMap.openCell(x, y);
+
+            it("в ячейку должно записаться 0", () =>{
+                expect(result).toBe(GameMapCell.Opened0)
+            })
+        })
+        
+        describe("С миной", () => {
+            gameMap.getCells()[y][x] = GameMapCell.Closed;    
+            spy = jest.spyOn(bombField, 'exists').mockImplementation(_ => true);
+
+            const result = gameMap.openCell(x, y);
+            
+            it("ячейка должна пометиться взрывом", () =>{
+                expect(result).toBe(GameMapCell.Boom)
+            });
+            spy?.mockRestore();
+        })
+    })    
 })
 
-describe("Работа с флагами", () => {
-    it("Мы успешно ставим флаг на закрытую ячейку", () => {
+describe("Установка флага", () => {
+
+    describe("Когда флаг ставится на закрытую ячейку", () => {
+        // Arrange
         const bombField = new BombField();
         const gameMap = new GameMap(bombField, 10, 13);
-        
         const x = 0, y = 0;
-        const result = gameMap.setFlag(x, y);
+        gameMap.getCells()[y][x] = GameMapCell.Closed;
 
-        expect(result).toBe(FlagResult.FlagAdded);
-        expect(gameMap.getCell(x, y)).toBe(GameMapCell.Flagged);
-    });
-    
+        // Act
+        const result = gameMap.setFlag(x, y);
+ 
+        // Assert
+        it("ячейка должна стать помеченной флагом", () => {
+            expect(gameMap.getCell(x, y)).toBe(GameMapCell.Flagged);    
+        })
+
+        it("должно быть уведомление об добавлении флага", () => {
+            expect(result).toBe(FlagResult.FlagAdded);
+        })
+    })
+
+    describe("Когда происходит попытка поставить флаг на открытую ячейку", () => {
+        // Arrange
+        const bombField = new BombField();
+        const x = 0, y = 0;
+
+        describe.each([ 
+            GameMapCell.Opened0, 
+            GameMapCell.Opened1, 
+            GameMapCell.Opened2,
+            GameMapCell.Opened3, 
+            GameMapCell.Opened4, 
+            GameMapCell.Opened5, 
+            GameMapCell.Opened6, 
+            GameMapCell.Opened7, 
+            GameMapCell.Opened8
+        ])("Когда количество соседних мин равно %s", cellValue => {
+            const gameMap = new GameMap(bombField, 5, 6);
+            gameMap.getCells()[y][x] = cellValue;
+
+            // Act
+            let result = gameMap.setFlag(x, y);
+
+            // Assert
+            it(`состояние ячейки не должно измениться`, () => {
+                expect(gameMap.getCell(x, y)).toBe(cellValue);
+            })
+
+            it("должно быть уведомление об отсутствии изменения количества флагов", () => {
+                expect(result).toBe(FlagResult.NoChanges);
+            })    
+        })
+})
+
+    describe("Когда происходит попытка поставить флаг на уже помеченную флагом ячейку", () => {
+        // Arrange
+        const bombField = new BombField();
+        const gameMap = new GameMap(bombField, 5, 6);
+        const x = 0, y = 0;
+        gameMap.getCells()[y][x] = GameMapCell.Flagged;
+
+        // Act
+        const result = gameMap.setFlag(x, y);
+ 
+        // Assert
+        it("флаг должен быть снят", () => {
+            expect(gameMap.getCell(x, y)).not.toBe(GameMapCell.Flagged);    
+        })
+
+        it("ячейка должна быть закрыта", () => {
+            expect(gameMap.getCell(x, y)).toBe(GameMapCell.Closed);    
+        })
+
+        it("должно быть уведомление о снятии флага", () => {
+            expect(result).toBe(FlagResult.FlagRemoved);
+        })
+    })
+
+    describe("Когда происходит попытка поставить флаг на взорванную ячейку", () => {
+        // Arrange
+        const bombField = new BombField();
+        const gameMap = new GameMap(bombField, 5, 6);
+        const x = 0, y = 0;
+        gameMap.getCells()[y][x] = GameMapCell.Boom;
+
+        // Act
+        const result = gameMap.setFlag(x, y);
+ 
+        // Assert
+        it("ячейка остаётся взорванной", () => {
+            expect(gameMap.getCell(x, y)).toBe(GameMapCell.Boom);    
+        })
+
+        it("должно быть уведомление об отсутствии изменения количества флагов", () => {
+            expect(result).toBe(FlagResult.NoChanges);
+        })
+
+    })
+})
+
+describe("Снятие флага", () => {
     it("Мы успешно можем снять флаг с ячейки с флагом", () => {
         const bombField = new BombField();
         const gameMap = new GameMap(bombField, 5, 6);
@@ -87,34 +237,4 @@ describe("Работа с флагами", () => {
         expect(result).toBe(FlagResult.FlagRemoved);
         expect(gameMap.getCell(x, y)).toBe(GameMapCell.Closed);
     });
-
-    it("Если попыться поставить флаг на незакрытую ячейку, ничего не происходит", () => {
-
-        const bombField = new BombField();
-        const gameMap = new GameMap(bombField, 5, 6);
-
-        const noChangesCellsValues: GameMapCell[] = [GameMapCell.Flagged, 
-                                                    GameMapCell.Opened0, 
-                                                    GameMapCell.Opened1, 
-                                                    GameMapCell.Opened2,
-                                                    GameMapCell.Opened3, 
-                                                    GameMapCell.Opened4, 
-                                                    GameMapCell.Opened5, 
-                                                    GameMapCell.Opened6, 
-                                                    GameMapCell.Opened7, 
-                                                    GameMapCell.Opened8, 
-                                                    GameMapCell.Boom];
-        
-        const x = 0, y = 0;
-        noChangesCellsValues.forEach(cellValue => {
-            gameMap.getCells()[y][x] = cellValue;
-
-            const result = gameMap.setFlag(x, y);
-
-            expect(result).toBe(FlagResult.NoChanges);
-            expect(gameMap.getCell(x, y)).toBe(cellValue);
-        })
-
-    }); 
-
-})
+}) 
